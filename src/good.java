4a4b90d0cb0494edd37b8c10725ef34439af4703
salmonmama <ninjinseijin00@yahoo.co.jp>
2021-01-13 02:38:29 +0900

    wwww#

--- src/good.java
+++ src/good.java
@@ -1,6 +1,6 @@
 public class Good {
     public static void main(String[] args) {
         System.out.println("Good");
-        System.out.println("aabbGood2");
+        System.out.println("Good2");
     }
 }
